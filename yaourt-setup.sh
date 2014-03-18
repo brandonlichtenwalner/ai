d9619e9e4e3a831f616009cad9b3e93c9a7beaa4
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-18 14:44:05 -0400

    Create yaourt-setup.sh

---
+++ yaourt-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# everyone uses the AUR, right?
+mkdir yaourt-setup
+cd yaourt-setup
+curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar zxvf package-query.tar.gz
+cd package-query
+makepkg -si
+cd ..
+curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar zxvf yaourt.tar.gz
+cd yaourt
+makepkg -si
+cd ..
+cd ..
+rm -r yaourt-setup
