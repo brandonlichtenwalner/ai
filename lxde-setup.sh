6ec6c83a63a20d98636bb898d936bc2a39d7fcfc
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 12:26:19 -0400

    Update lxde-setup.sh

--- lxde-setup.sh
+++ lxde-setup.sh
@@ -7,10 +7,8 @@ sudo pacman -Syu chromium geany-plugins gvfs lxde
 
 sudo systemctl enable lxdm
 
-# other "required" tools
-sudo pacman -S kdebase-okular
-
+# All of the extras for chromium
 yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
 
 # additional applications plus some useful optional dependencies
-# pacman -S alsa-plugins dosfstools gvfs-smb ntfs-3g steam vlc ffmpeg speex
+# pacman -S alsa-plugins dosfstools gvfs-smb kdebase-okular ntfs-3g steam vlc ffmpeg speex
