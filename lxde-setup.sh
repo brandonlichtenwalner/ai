1a26005b5cd6dba67e4237f6ae0c136d748e2c4c
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 12:24:49 -0400

    Update lxde-setup.sh

--- lxde-setup.sh
+++ lxde-setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # LXDE with my commonly used applications
+# run as a user so yaourt can be called as non-root
 
-pacman -Syu chromium geany-plugins gvfs lxde
+sudo pacman -Syu chromium geany-plugins gvfs lxde
 
-systemctl enable lxdm
+sudo systemctl enable lxdm
+
+# other "required" tools
+sudo pacman -S kdebase-okular
+
+yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
 
 # additional applications plus some useful optional dependencies
-# pacman -S alsa-plugins gvfs-smb steam vlc ffmpeg speex
+# pacman -S alsa-plugins dosfstools gvfs-smb ntfs-3g steam vlc ffmpeg speex
