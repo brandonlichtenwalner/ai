b22f30e7a57c8720d6b7490e123ef14ac4a22755
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 11:54:31 -0400

    Update lxde-custom.sh

--- lxde-custom.sh
+++ lxde-custom.sh
@@ -5,16 +5,12 @@
 # Replaced: lxdm with lightdm-gtk3-greeter
 #
 # Removed: gpicview lxmusic lxpanel
+#
+# Added: chromium gvfs geany-plugins
 
-pacman -Syu lightdm-gtk3-greeter lxapparance-obconf lxde-common lxde-icon-theme lxinput lxmenu-data lxpolkit lxrandr lxsession lxshortcut lxtask lxterminal menu-cache openbox pcmanfm
-
-# optional, but desired dependencies
-pacman -S gvfs-smb
+pacman -Syu chromium geany-plugins gvfs lightdm-gtk3-greeter lxapparance-obconf lxde-common lxde-icon-theme lxinput lxmenu-data lxpolkit lxrandr lxsession lxshortcut lxtask lxterminal menu-cache openbox pcmanfm
 
 systemctl enable lightdm
 
-# additional applications everyone needs
-pacman -S chromium geany
-
-# even more apps that you probably want
-# pacman -S steam vlc
+# additional applications plus some useful optional dependencies
+# pacman -S alsa-plugins gvfs-smb steam vlc ffmpeg speex 
