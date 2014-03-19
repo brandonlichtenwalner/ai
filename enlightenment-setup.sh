24304c38b2b9bee25274e1b4be52aeb0dba940b5
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 12:11:35 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-pacman -Syu enlightenment lightdm-gtk-greeter terminology
+# Enlightenment with LightDM and my commonly used applications 
+
+pacman -Syu chromium enlightenment geany-plugins gvfs lightdm-gtk-greeter
 
 systemctl enable lightdm
+
+# lxterminal or terminology
+# pcmanfm if the default file manager is still crap
