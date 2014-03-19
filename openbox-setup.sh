b4fca738840c6361538233b8fee3e9d8ec405275
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 15:40:12 -0400

    Update openbox-setup.sh

--- openbox-setup.sh
+++ openbox-setup.sh
@@ -26,6 +26,7 @@ sudo systemctl enable lightdm
 # additional applications plus some useful optional dependencies
 # pacman -S alsa-plugins dosfstools ffmpeg gvfs-smb libdvdcss lxappearance-obconf lxrandr ntfs-3g speex steam vlc
 
+echo :::
 echo Please see https://wiki.archlinux.org/index.php/Openbox#GTK.2B_2 if having problems with GTK themes.
 echo :::
 echo You might want to reboot now to enjoy your fresh Openbox GUI.
