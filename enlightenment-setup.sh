2b0106b1018ba2b778ed93e801b163079a1f865d
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 21:27:41 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -3,22 +3,16 @@
 # Enlightenment with LightDM and my commonly used applications
 # Set up to run as a normal user with sudo
 
-sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring lightdm-gtk3-greeter polkit-gnome terminology
+sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring gvs lightdm-gtk3-greeter pcmanfm polkit-gnome terminology
 
 sudo systemctl enable lightdm
 sudo systemctl enable polkit-gnome
 
-# Install all of the extras for chromium
-yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
+# Install all of the extras for chromium plus a few other AUR packages
+yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin spideroak virtualbox-ext-oracle
 
 # additional applications plus some useful optional dependencies
-sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer lxappearance-obconf lxrandr mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip vlc vorbis-tools
-
-# if efm stilll sucks
-# sudo pacman -S gvfs-smb pcmanfm
-
-# others, might be AUR
-# spideroak virtualbox-ext-oracle virtualbox-guest-iso
+sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso vlc vorbis-tools
 
 echo :::
 echo Please see per-user GNOME Keyring fix at https://wiki.archlinux.org/index.php/enlightenment#GNOME_Keyring_integration
