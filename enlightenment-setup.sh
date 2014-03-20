8f9f4191723f8f77d5154f31e8e8bbdc38506797
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-20 09:03:21 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -4,7 +4,7 @@
 # Set up to run as a normal user with sudo
 echo :::
 echo Installing basic Enlightenment environment
-sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring gvs lightdm-gtk3-greeter pcmanfm polkit-gnome terminology
+sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring gvfs lightdm-gtk3-greeter pcmanfm polkit-gnome terminology
 
 sudo systemctl enable lightdm
 sudo systemctl enable polkit-gnome
@@ -17,7 +17,7 @@ yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin spideroak v
 # additional applications plus some useful optional dependencies
 echo :::
 echo Installing applications for a complete desktop experience plus useful optional add-ons
-sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso vlc vorbis-tools
+sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer mumble ntfs-3g p7zip speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso vlc vorbis-tools
 
 echo :::
 echo Please see per-user GNOME Keyring fix at https://wiki.archlinux.org/index.php/enlightenment#GNOME_Keyring_integration
