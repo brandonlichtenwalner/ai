984aab09bd46bb8bd9368502cdf3e51c88b3a7ec
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-20 08:59:04 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -2,16 +2,21 @@
 
 # Enlightenment with LightDM and my commonly used applications
 # Set up to run as a normal user with sudo
-
+echo :::
+echo Installing basic Enlightenment environment
 sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring gvs lightdm-gtk3-greeter pcmanfm polkit-gnome terminology
 
 sudo systemctl enable lightdm
 sudo systemctl enable polkit-gnome
 
 # Install all of the extras for chromium plus a few other AUR packages
+echo :::
+echo Installing Chromium extras,Spideroak, and Virtualbox extensions from AUR
 yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin spideroak virtualbox-ext-oracle
 
 # additional applications plus some useful optional dependencies
+echo :::
+echo Installing applications for a complete desktop experience plus useful optional add-ons
 sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso vlc vorbis-tools
 
 echo :::
