87deb8955ad19f53473741f15fde7b1765f1c608
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 20:54:56 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Enlightenment with LightDM and my commonly used applications 
+# Enlightenment with LightDM and my commonly used applications
+# Set up to run as a normal user with sudo
 
-pacman -Syu chromium enlightenment geany-plugins gvfs lightdm-gtk-greeter
+sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring lightdm-gtk3-greeter terminology
 
-systemctl enable lightdm
+themes?
 
-# lxterminal or terminology
-# pcmanfm if the default file manager is still crap
+sudo systemctl enable lightdm
+
+
+
+# Install all of the extras for chromium
+# yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
+
+# additional applications plus some useful optional dependencies
+# pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer lxappearance-obconf lxrandr mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip vlc vorbis-tools
+
+# if efm stilll sucks
+# sudo pacman -S gvfs-smb pcmanfm
+
+# others, might be AUR
+# spideroak virtualbox-ext-oracle virtualbox-guest-iso
