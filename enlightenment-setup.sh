91342d6eb87accd68da867603faf74370e9e12ca
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 21:03:23 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -8,17 +8,17 @@ sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gn
 sudo systemctl enable lightdm
 sudo systemctl enable polkit-gnome
 
-echo :::
-echo Please see per-user GNOME Keyring fix at https://wiki.archlinux.org/index.php/enlightenment#GNOME_Keyring_integration
-
 # Install all of the extras for chromium
-# yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
+yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
 
 # additional applications plus some useful optional dependencies
-# pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer lxappearance-obconf lxrandr mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip vlc vorbis-tools
+sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility hplip libdvdcss libreoffice-calc libreoffice-impress libreoffice-writer lxappearance-obconf lxrandr mumble ntfs-3g speex steam system-config-printer transmission-gtk unrar unzip vlc vorbis-tools
 
 # if efm stilll sucks
 # sudo pacman -S gvfs-smb pcmanfm
 
 # others, might be AUR
 # spideroak virtualbox-ext-oracle virtualbox-guest-iso
+
+echo :::
+echo Please see per-user GNOME Keyring fix at https://wiki.archlinux.org/index.php/enlightenment#GNOME_Keyring_integration
