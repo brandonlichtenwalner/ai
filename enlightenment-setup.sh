c5ca77f5a566ed42163f6bbeeea4f98cc4a5dc4e
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-19 21:00:05 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -3,13 +3,13 @@
 # Enlightenment with LightDM and my commonly used applications
 # Set up to run as a normal user with sudo
 
-sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring lightdm-gtk3-greeter terminology
-
-themes?
+sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring lightdm-gtk3-greeter polkit-gnome terminology
 
 sudo systemctl enable lightdm
+sudo systemctl enable polkit-gnome
 
-
+echo :::
+echo Please see per-user GNOME Keyring fix at https://wiki.archlinux.org/index.php/enlightenment#GNOME_Keyring_integration
 
 # Install all of the extras for chromium
 # yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin
