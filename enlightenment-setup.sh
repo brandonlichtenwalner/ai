e1f171025b43be7c8cd34f838b89b1303f21bd67
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-22 17:52:58 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -2,21 +2,25 @@
 
 # Enlightenment with LightDM and my commonly used applications
 # Set up to run as a normal user with sudo
+
+echo :::
+echo Making sure all packages are up to date...
+sudo pacman -Syu
 echo :::
 echo Installing basic Enlightenment environment
-sudo pacman -Syu chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring gvfs lightdm-gtk3-greeter lxpolkit pcmanfm terminology
+sudo pacman -S chromium enlightenment geany-plugins gnome-icon-theme-extras gnome-keyring gvfs lightdm-gtk3-greeter lxpolkit pcmanfm terminology
 
 sudo systemctl enable lightdm
-
-# Install all of the extras for chromium plus a few other AUR packages
-echo :::
-echo Installing Chromium extras, Spideroak, and Virtualbox extensions from AUR
-yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin spideroak virtualbox-ext-oracle
 
 # additional applications plus some useful optional dependencies
 echo :::
 echo Installing applications for a complete desktop experience plus useful optional add-ons
-sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hplip hunspell-en hypehn-en libdvdcss libreoffice-calc libreoffice-en-US libreoffice-gnome libreoffice-impress libreoffice-writer mumble mythes mythes-en ntfs-3g p7zip speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso vlc vorbis-tools
+sudo pacman -S alsa-plugins arandr asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hplip hunspell-en hyphen-en libdvdcss libreoffice-calc libreoffice-en-US libreoffice-gnome libreoffice-impress libreoffice-writer mumble mythes mythes-en ntfs-3g p7zip qt4 speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso vlc vorbis-tools
+
+# Install all of the extras for chromium plus a few other AUR packages
+echo :::
+echo Installing Chromium extras and Virtualbox extensions from AUR
+yaourt -Syua chromium-pepper-flash chromium-libpdf google-talkplugin virtualbox-ext-oracle
 
 # on a laptop or other device where you want to set up wireless
 # sudo pacman -S network-manager-applet
@@ -25,3 +29,5 @@ sudo pacman -S alsa-plugins asunder baobab brasero dosfstools ffmpeg file-roller
 
 echo :::
 echo Please see the enlightenment-post-install.txt file for additional--per user--configuration steps.
+
+rm enlightenment-setup.sh
