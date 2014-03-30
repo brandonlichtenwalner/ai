e826d0bd2d4a9c02fa7240b103388a517d61f968
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-03-30 16:51:39 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -14,6 +14,10 @@ VIDEO=""
 LAPTOP=""
 #LAPTOP="xf86-input-synaptics"
 
+# if running in Virtualbox uncomment the following and then remember to run it (as root)
+#wget https://raw.github.com/brandonlichtenwalner/ai/master/vbox-guest-setup.sh
+#chmod +x vbox-guest-setup.sh
+
 # the first user is assumed to be an admin user
 useradd -m -G users,games,wheel -s /bin/bash $USER1
 echo Set password for $USER1
