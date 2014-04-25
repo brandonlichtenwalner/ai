dd7fbab80d78a044d2c63abd9b3294140e8a500a
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-04-24 20:36:07 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -47,6 +47,7 @@ yaourt -S acroread
 # on a laptop or other device where you want to set up wireless
 # sudo pacman -S network-manager-applet
 # sudo systemctl enable NetworkManager.service
+# sudo systemctl disable dhcpcd.service
 
 
 echo :::
