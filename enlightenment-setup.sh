92486a3efd447ed2ccf4cde90418dbf1604dd1e6
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-04-28 10:12:17 -0400

    Update enlightenment-setup.sh

--- enlightenment-setup.sh
+++ enlightenment-setup.sh
@@ -17,11 +17,17 @@ echo :::
 echo Installing applications for a complete desktop experience plus useful optional add-ons
 sudo pacman -S alsa-plugins arandr asunder baobab brasero dosfstools ffmpeg file-roller gnome-disk-utility gvfs-smb hunspell-en hyphen-en lib32-alsa-plugins libdvdcss libreoffice-calc libreoffice-en-US libreoffice-gnome libreoffice-impress libreoffice-writer mumble mythes mythes-en ntfs-3g p7zip qt4 shotwell speex steam system-config-printer transmission-gtk unrar unzip virtualbox-guest-iso virtualbox-host-modules vlc vorbis-tools
 
-# for a print server (i.e. local printer attached)
+# for general desktop printing (i.e. where there is not already a print server set up elsewhere)
+# assumes you are using an HP printer
+echo :::
+echo Installing CUPS with HPLIP and enabling the service
+sudo pacman -S cups hplip
+sudo systemctl enable cups.service
+
+# If just connecting to a print server
 # echo :::
-# echo Installing CUPS and enabling the service
-# sudo pacman -S cups hplip
-# sudo systemctl enable cups.service
+# echo Installing CUPS (client-only)
+# sudo pacman -S libcups
 
 # SANE stuff, if applicable, would go here
 
