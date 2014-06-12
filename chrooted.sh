ab68a23ebf15b1e3ff5c21877cae176e80cc1925
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-06-12 16:57:40 -0400

    Update chrooted.sh

--- chrooted.sh
+++ chrooted.sh
@@ -5,13 +5,12 @@
 
 # variables
 LANGUAGE=en_US.UTF-8
-SLEEPTIME=5
 HOSTNAME=arch
 TZ=America/New_York
 
-echo You need to uncomment $LANGUAGE in /etc/locale.gen
-echo launching nano in $SLEEPTIME seconds...
-sleep $SLEEPTIME
+echo :::
+echo Preparing to open /etc/locale.gen for editing--you need to uncomment $LANGUAGE
+read -p "Press [Enter] to continue."
 nano /etc/locale.gen
 
 locale-gen
@@ -24,34 +23,39 @@ hwclock --systohc --utc
 
 echo $HOSTNAME > /etc/hostname
 
+echo :::
 echo About to enable dhcpcd.service
 echo If using wireless or a non-standard network config (during setup) hit Control+Z now to halt the script and finish the process manually.
-sleep $SLEEPTIME
+read -p "Press [Enter] to continue."
 systemctl enable dhcpcd.service
 
+echo :::
 echo About to run mkinitcpio
 echo If you need to add any HOOKS--e.g. RAID, LVM, or USB boot--hit Control+Z now to halt the script and finish the process manually.
-sleep $SLEEPTIME
+read -p "Press [Enter] to continue."
 # nano /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
+echo :::
 echo Set the root password...
 passwd
 
+echo :::
 echo Proceeding with a standard Syslinux bootloader install
 echo If you want to use GRUB, etc. hit Control+Z now to stop halt the script and finish the process manually.
-sleep $SLEEPTIME
+read -p "Press [Enter] to continue."
 pacman -S gptfdisk syslinux
 syslinux-install_update -i -a -m
 
+echo :::
 echo Time to configure syslinux.cfg :: change /dev/sda3 to $ROOTPART and edit any other options to your liking.
-echo launching nano in $SLEEPTIME seconds...
-sleep $SLEEPTIME
+read -p "Press [Enter] to continue."
 nano /boot/syslinux/syslinux.cfg
 
 echo :::
-echo Removing chrooted.sh in $SLEEPTIME seconds.
-sleep $SLEEPTIME
+echo Cleaning up: removing chrooted.sh
+read -p "Press [Enter] to continue."
 rm /chrooted.sh
 
+echo :::
 echo Now just type -exit- to leave the chroot environment and finish installation.
