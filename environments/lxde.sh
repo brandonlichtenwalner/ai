34a1f640609801c1afe7a0067bc576c7052b936a
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 15:08:24 -0400

    Update lxde.sh

--- environments/lxde.sh
+++ environments/lxde.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Custom LXDE with Cairo-Dock
+
+# Set up to run as a normal user with sudo
+
+echo :::
+echo Making sure all packages are up to date...
 yaourt -Syua
-sudo pacman -S cairo-dock-plugins dosfstools ffmpeg gnome-menus gnome-icon-theme gvfs gvfs-smb libical lxappearance lxappearance-obconf lxde-common lxde-icon-theme lxdm lxinput lxmenu-data lxpolkit lxrandr lxsession lxtask lxterminal menu-cache openbox ntfs-3g pcmanfm smbclient xcompmgr
 
-# additional packages
-# sudo pacman -S lm_sensors lxmusic
-
-# Cairo-Dock Tweak thread for LXDE
-# http://www.remastersys.com/forums/index.php?topic=1734.0
+echo :::
+echo Installing custom LXDE environment
+sudo pacman -S cairo-dock-plugins gnome-icon-theme-extras gnome-menus gvfs libical lxappearance lxappearance-obconf lxde-common lxde-icon-theme lxdm lxinput lxmenu-data lxpolkit lxrandr lxsession lxtask lxterminal menu-cache openbox pcmanfm xcompmgr
 
 sudo systemctl enable lxdm
 
@@ -15,3 +18,12 @@ mkdir ~/.config/lxsession
 mkdir ~/.config/lxsession/LXDE
 echo '@xcompmgr -c' >> ~/.config/lxsession/LXDE/autostart
 echo '@cairo-dock -o' >> ~/.config/lxsession/LXDE/autostart
+
+echo :::
+echo Note: The LXDE Icon is located at /usr/share/lxde/images/lxde-icon.png
+
+# Cairo-Dock Tweak thread for LXDE
+# http://www.remastersys.com/forums/index.php?topic=1734.0
+
+# To enable system monitor plugin in Cairo-Dock
+# sudo pacman -S lm_sensors
