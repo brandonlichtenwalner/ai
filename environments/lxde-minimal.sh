9bd86f511f4a8aafe3603cf2d71b5d25d28d04cf
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 11:39:21 -0400

    Update lxde-minimal.sh

--- environments/lxde-minimal.sh
+++ environments/lxde-minimal.sh
@@ -3,4 +3,7 @@
 yaourt -Syua
 sudo pacman -S gvfs lxappearance lxappearance-obconf lxde-common lxde-icon-theme lxdm lxinput lxmenu-data lxpolkit lxrandr lxsession lxtask lxterminal menu-cache openbox pcmanfm
 
+#optional
+# sudo pacman -S dosfstools gvfs-smb ntfs-3g
+
 sudo systemctl enable lxdm
