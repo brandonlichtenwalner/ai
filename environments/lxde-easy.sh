233ed887470101c4c8b3db301dfb029100eef61c
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 14:18:40 -0400

    Create lxde-easy.sh

---
+++ environments/lxde-easy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yaourt -Syua
+sudo pacman -S cairo-dock-plugins dosfstools ffmpeg gnome-menus gvfs gvfs-smb lxde ntfs-3g smbclient xcompmgr
+
+# additional cairo-dock plugins
+# sudo pacman -S libical lm_sensors
+
+# Cairo-Dock Tweak thread for LXDE
+# http://www.remastersys.com/forums/index.php?topic=1734.0
+
+sudo systemctl enable lxdm
+
+mkdir ~/.config/lxsession
+mkdir ~/.config/lxsession/LXDE
+echo '@xcompmgr -c' >> ~/.config/lxsession/LXDE/autostart
+echo '@cairo-dock -o' >> ~/.config/lxsession/LXDE/autostart
