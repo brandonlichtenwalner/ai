3e181a7fecf499ec39cd79da26a0ae36cb5ae47b
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 13:10:11 -0400

    Update lxde.sh

--- environments/lxde.sh
+++ environments/lxde.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 yaourt -Syua
-sudo pacman -S cairo-dock dosfstools ffmpeg gvfs gvfs-smb lxde ntfs-3g smbclient
+sudo pacman -S cairo-dock dosfstools ffmpeg gvfs gvfs-smb lxde ntfs-3g smbclient xcompmgr
+
+# additional cairo-dock plugins
+# sudo pacman -S libical lm_sensors
 
 sudo systemctl enable lxdm
+
 mkdir ~/.config/lxsession
 mkdir ~/.config/lxsession/LXDE
-echo '@cairo-dock -f -c' >> ~/.config/lxsession/LXDE/autostart
+echo '@xcompmgr -c' >> ~/.config/lxsession/LXDE/autostart
+echo '@cairo-dock -o' >> ~/.config/lxsession/LXDE/autostart
