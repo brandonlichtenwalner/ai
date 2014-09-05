46b4834b97802800b90efb4e3cb7e2e450a01b5f
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 11:17:50 -0400

    Create lxde-minimal.sh

---
+++ environments/lxde-minimal.sh
@@ -0,0 +1,4 @@
+yaourt -Syua
+sudo pacman -S lxappearance lxappearance-obconf lxde-common lxde-icon-theme lxdm lxinput lxmenu-data lxpanel lxpolkit lxrandr lxsession lxtask lxterminal menu-cache openbox pcmanfm
+
+sudo systemctl enable lxdm
