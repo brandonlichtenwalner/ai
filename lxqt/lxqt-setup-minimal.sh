f8c8a07cb297152429f45f93b53e22728c6fc537
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 09:41:34 -0400

    Update and rename lxqt-setup.sh to lxqt-setup-minimal.sh

---
+++ lxqt/lxqt-setup-minimal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# LXQt with LightDM
+# Set up to run as a normal user with sudo
+
+echo :::
+echo Making sure all packages are up to date...
+yaourt -Syua
+echo :::
+echo Installing basic LXQt environment
+yaourt -S lxqt-desktop-git pcmanfm-qt-git openbox qterminal-get obconf-qt-git lxqt-lightdm-greeter-git oxygen-icons qtcurve-gtk2 qtcurve-qt4 qtcurve-qt5 qtcurve-utils
+
+sudo systemctl enable lightdm
+
+rm lxqt-setup-minimal.sh
