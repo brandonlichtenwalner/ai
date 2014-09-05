0cc1bb2299ec76a8f6e9755499c82f46fbaab110
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-05 16:47:59 -0400

    Update enlightenment-setup.sh

--- environments/enlightenment-setup.sh
+++ environments/enlightenment-setup.sh
@@ -2,17 +2,11 @@
 
 # Enlightenment with LightDM
 
-# Set up to run as a normal user with sudo
-
-echo :::
-echo Making sure all packages are up to date...
-yaourt -Syua
-
 echo :::
 echo Installing basic Enlightenment environment
-sudo pacman -S arandr enlightenment gnome-icon-theme-extras gnome-keyring gnome-themes-standard gvfs lightdm-gtk3-greeter lxpolkit numlockx pcmanfm terminology
+pacman -S arandr enlightenment gnome-icon-theme-extras gnome-keyring gnome-themes-standard gvfs lightdm-gtk3-greeter lxpolkit numlockx pcmanfm terminology
 
-sudo systemctl enable lightdm
+systemctl enable lightdm
 
 echo :::
 echo Please see the enlightenment-post-install.txt file for additional--per user--configuration steps.
