d07c5ffe46e1254c3c297e8f4a990eca1c9ae83c
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-09-06 09:55:20 -0400

    more lxde less cairo-dock
    switched to full lxde instead of instead of individual packages
    removed cairo-dock plugins and all optional dependencies in favor of just cairo-dock (some plugins were crashy) and added the -f flag to cairo-dock in users autostart files

--- environments/lxde-setup.sh
+++ environments/lxde-setup.sh
@@ -4,7 +4,7 @@
 
 echo :::
 echo Installing custom LXDE environment
-pacman -S cairo-dock-plugins gnome-icon-theme-extras gnome-menus gvfs libical lxappearance lxappearance-obconf lxde-common lxde-icon-theme lxdm lxinput lxmenu-data lxpolkit lxrandr lxsession lxtask lxterminal menu-cache openbox pcmanfm xcompmgr
+pacman -S cairo-dock gnome-icon-theme-extras gvfs lxde xcompmgr
 
 # To enable system monitor plugin in Cairo-Dock
 # sudo pacman -S lm_sensors
@@ -17,13 +17,13 @@ echo "Configuring xcompmgr and cairo-dock to autostart for user(s)..."
 mkdir /home/$USER1/.config/lxsession
 mkdir /home/$USER1/.config/lxsession/LXDE
 echo '@xcompmgr -c' >> /home/$USER1/.config/lxsession/LXDE/autostart
-echo '@cairo-dock -c' >> /home/$USER1/.config/lxsession/LXDE/autostart
+echo '@cairo-dock -c -f' >> /home/$USER1/.config/lxsession/LXDE/autostart
 
 if [ "$USER2" != "" ]; then
   mkdir /home/$USER2/.config/lxsession
   mkdir /home/$USER2/.config/lxsession/LXDE
   echo '@xcompmgr -c' >> /home/$USER2/.config/lxsession/LXDE/autostart
-  echo '@cairo-dock -c' >> /home/$USER2/.config/lxsession/LXDE/autostart
+  echo '@cairo-dock -c -f' >> /home/$USER2/.config/lxsession/LXDE/autostart
 fi
 
 echo :::
