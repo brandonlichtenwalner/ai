95290e64bc70b88a747d64030631c71d4bd3a37a
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-10-14 12:38:24 -0400

    Update lxde-minimal-setup.sh

--- environments/lxde-minimal-setup.sh
+++ environments/lxde-minimal-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 yaourt -Syua
 sudo pacman -S gvfs lxappearance lxappearance-obconf lxde-common lxde-icon-theme lxdm lxinput lxmenu-data lxpolkit lxrandr lxsession lxtask lxterminal menu-cache openbox pcmanfm
