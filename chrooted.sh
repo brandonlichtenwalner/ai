a56a5a8994ffd717881777301b7991a0ce42922c
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-12-15 00:37:38 -0500

    Update chrooted.sh

--- chrooted.sh
+++ chrooted.sh
@@ -3,12 +3,11 @@
 # This script follows https://wiki.archlinux.org/index.php/Beginners'_guide
 # Before running it, make sure you have already run setup.sh (or otherwise performed those steps).
 
-# variables
+# variables that don't change for me
 LANGUAGE=en_US.UTF-8
-HOSTNAME=arch
 TZ=America/New_York
 
-echo :::
+echo ":::"
 echo "Preparing to open /etc/locale.gen for editing--you need to uncomment $LANGUAGE"
 read -p "Press [Enter] to continue."
 nano /etc/locale.gen
@@ -21,41 +20,48 @@ ln -s /usr/share/zoneinfo/$TZ /etc/localtime
 
 hwclock --systohc --utc
 
+echo ":::"
+echo "Please enter a hostname, e.g. 'arch': "
+read HOSTNAME
+
 echo $HOSTNAME > /etc/hostname
 
-echo :::
+echo ":::"
 echo "About to enable dhcpcd.service"
 echo "If using wireless or a non-standard network config (during setup) hit Control+Z now to halt the script and finish the process manually."
 read -p "Press [Enter] to continue."
 systemctl enable dhcpcd.service
 
-echo :::
+echo ":::"
 echo "About to run mkinitcpio"
-echo "If you need to add any HOOKS--e.g. RAID, LVM, or USB boot--hit Control+Z now to halt the script and finish the process manually."
-read -p "Press [Enter] to continue."
-# nano /etc/mkinitcpio.conf
+echo "Type 'yes' if you need to enter any HOOKS--e.g. for RAID, LVM, or USB boot--or hit [Enter] to continue: "
+read HOOKS
+if [ HOOKS = "yes" ]; then
+  nano /etc/mkinitcpio.conf
+fi
+
 mkinitcpio -p linux
 
-echo :::
+echo ":::"
 echo "Set the root password..."
 passwd
 
-echo :::
-echo Proceeding with a standard Syslinux bootloader install
-echo If you want to use GRUB, etc. hit Control+Z now to stop halt the script and finish the process manually.
+echo ":::"
+echo "Proceeding with a standard Syslinux bootloader install"
+echo "If you want to use GRUB, etc. hit Control+Z now to stop halt the script and finish the process manually."
 read -p "Press [Enter] to continue."
 pacman -S gptfdisk syslinux
 syslinux-install_update -i -a -m
 
-echo :::
-echo Time to configure syslinux.cfg :: change /dev/sda3 to $ROOTPART and edit any other options to your liking.
+echo ":::"
+echo "Time to configure syslinux.cfg :: change the root partition as needed and edit any other options to your liking."
 read -p "Press [Enter] to continue."
 nano /boot/syslinux/syslinux.cfg
 
-echo :::
-echo Cleaning up: removing chrooted.sh
+echo ":::"
+echo "Cleaning up: removing chrooted.sh"
 read -p "Press [Enter] to continue."
 rm /chrooted.sh
 
-echo :::
-echo Now just type -exit- to leave the chroot environment and finish installation.
+echo ":::"
+echo "Now just type 'exit' to leave the chroot environment and finish installation."
