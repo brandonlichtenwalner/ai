1ca52bf83b7ffb8bd9afe2920eb3d4aeaa485986
Brandon <brandonlichtenwalner@users.noreply.github.com>
2014-12-14 23:46:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,17 +10,17 @@ echo " 4) mounted the root (/) partition at /mnt, e.g. 'mount /dev/sda1 /mnt'"
 # See: https://wiki.archlinux.org/index.php/Suspend_and_hibernate#About_swap_partition.2Ffile_size for SWAP size logic
 read -p "Press [Enter] to begin."
 
-echo :::
-echo It will take a few minutes--or longer on a slow connection--for pacstrap to run,
-echo so be patient and do not touch anything if you want this script to finish successfully!
+echo ":::"
+echo "It will take a few minutes--or longer on a slow connection--for pacstrap to run,"
+echo "so be patient and do not touch anything if you want this script to finish successfully!"
 pacstrap /mnt base
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
 # download chrooted.sh and post-install.sh into /mnt
 cd /mnt
-echo :::
-echo Grabbing the chroot and post-installation scripts...
+echo ":::"
+echo "Grabbing the chroot and post-installation scripts..."
 wget https://github.com/brandonlichtenwalner/arch-install/raw/master/chrooted.sh
 chmod +x chrooted.sh
 cd /mnt/root
@@ -28,12 +28,12 @@ wget https://github.com/brandonlichtenwalner/arch-install/raw/master/post-instal
 chmod +x post-install.sh
 cd
 
-echo :::
-echo Entering chroot environment where you must run chrooted.sh to continue...
+echo ":::"
+echo "Entering chroot environment where you must run chrooted.sh to continue..."
 arch-chroot /mnt /bin/bash
 # the system will now be waiting for the arch-chroot to finish
 
 # once the chroot is exited finish up with unmounting
 umount -R /mnt
-echo :::
-echo Your basic Arch Linux installation is now complete. Reboot and continue with /root/post-install.sh if desired.
+echo ":::"
+echo "Your basic Arch Linux installation is now complete. Reboot and continue with /root/post-install.sh if desired."
