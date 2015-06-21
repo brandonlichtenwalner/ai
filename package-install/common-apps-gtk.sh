d27c06398dfb25ada8a0fedc3df098808da5e9f6
Brandon <brandonlichtenwalner@users.noreply.github.com>
2015-06-21 16:46:29 -0400

    Update common-apps-gtk.sh

--- package-install/common-apps-gtk.sh
+++ package-install/common-apps-gtk.sh
@@ -3,3 +3,5 @@
 echo ":::"
 echo "Installing set of commonly used GTK applications"
 sudo pacman -S brasero chromium ffmpeg gvfs-smb hunspell-en hyphen-en libdvdcss libreoffice-still-calc libreoffice-still-en-US libreoffice-still-gnome libreoffice-still-impress libreoffice-still-writer mythes mythes-en ntfs-3g p7zip rubyripper shutter smbclient transmission-gtk unrar unzip vlc
+
+rm common-apps-gtk.sh
