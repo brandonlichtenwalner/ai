ef4b3d67084060c488bdc444576c8144d389e160
Brandon <brandonlichtenwalner@users.noreply.github.com>
2015-06-21 16:29:23 -0400

    Create network-manager-applet.sh

---
+++ package-install/network-manager-applet.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# on a laptop or other device where you want to set up wireless
+sudo pacman -S network-manager-applet
+sudo systemctl enable NetworkManager.service
+sudo systemctl disable dhcpcd.service
