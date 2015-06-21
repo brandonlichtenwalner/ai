b634f62f6b5633da6281b1c641b38ea5c37f7f25
Brandon <brandonlichtenwalner@users.noreply.github.com>
2015-06-21 16:33:51 -0400

    Create virtualbox.sh

---
+++ package-install/virtualbox.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo ":::"
+echo "Installing and configuring Virtualbox"
+sudo pacman -S qt4 virtualbox-guest-iso virtualbox-host-modules
+
+# load vboxdrv module on startup
+sudo sh -c "echo \"vboxdrv\" > /etc/modules-load.d/virtualbox.conf"
+
+echo ":::"
+echo "Installing Virtualbox extensions from AUR"
+yaourt -S virtualbox-ext-oracle
