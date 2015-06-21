2838d3cc020e6e706ca2654f4e26726d1ef644c7
Brandon <brandonlichtenwalner@users.noreply.github.com>
2015-06-21 17:30:03 -0400

    Update network-manager-applet.sh

--- package-install/network-manager-applet.sh
+++ package-install/network-manager-applet.sh
@@ -2,7 +2,5 @@
 
 # on a laptop or other device where you want to set up wireless
 sudo pacman -S network-manager-applet
-sudo systemctl enable NetworkManager.service
 sudo systemctl disable dhcpcd.service
-
-rm network-manager-applet.sh
+sudo systemctl enable NetworkManager.service
